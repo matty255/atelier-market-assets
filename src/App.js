987badaba2d04a553d065db0082052133bcbe663
matty255 <89088205+matty255@users.noreply.github.com>
2022-02-12 21:17:01 +0900

    저장

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import tw from "tailwind-styled-components"
+import BucketList from './BucketList';
+import Header from './Header';
+import { Route, Link } from "react-router-dom";
+
+
+const Container = tw.div`
+    flex
+    items-center
+    justify-center
+    flex-col
+    w-64
+    h-64
+    m-auto
+    bg-indigo-600
+    hover:bg-red-400
+`
+
+const Container2 = tw.div`
+    w-full m-auto bg-purple-500
+    cursor-pointer
+    rounded-xl
+    animate-bounce
+`
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Link to="/">홈으로 가기</Link>
+        <Link to="/header/:head_name">헤더로 가기</Link>
+      </div>
+    <Container>
+          <h1 className="text-3xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
+    
+    </Container>
+    <Container2>
+    <Route path="/" exact component={BucketList} />
+    <Route path="/header/:head_name" component={Header} />
+    </Container2>
     </div>
   );
 }
